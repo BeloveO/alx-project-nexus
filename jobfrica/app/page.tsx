e7e0a9fdb7472aa0430dc50f77995e7c7cc1f8ce
theisraelolaleye <theisraelolaleye@gmail.com>
2025-11-12 11:36:41 +0100

    main page

--- jobfrica/app/page.tsx
+++ jobfrica/app/page.tsx
@@ -2,6 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <main>landing page</main>
+    <main>landing page
+      
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo reiciendis sunt id facere sed porro a iure eos architecto maiores odio quasi, quam aperiam dignissimos repellendus. Excepturi quae ex quam!
+   </main>
   );
 }
